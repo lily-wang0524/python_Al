0a82c94a69222993e6675378b1b1e489307f9102
Limin <850778345@qq.com>
2023-04-09 21:01:48 +0800

    模拟文件系统-树

--- p67.py
+++ p67.py
@@ -1,4 +1,3 @@
-
 # 树的实例：模拟文件系统
 # 树是链式存储
 class Node(): # 创建文件节点的类，及其属性（父节点，孩子节点）
@@ -26,6 +25,38 @@ class FileSystemTree(): # 创建文件根目录——数据结构（树）
         self.now.children.append(new_dir)  
         new_dir.parent = self.now 
 
-tree = FileSystemTree() # 类实例化，新建目录树
-tree.mkdir("Var/") # 新建文件夹
-print(tree.root.children)
+    def ls(self):  # 展现当前目录下的所有子目录
+        return self.now.children  # 返回子目录列表
+    
+    def cd(self,name): # 切换目录(到子目录)，支持向上返回
+        # 判断是否为文件夹
+        if name[-1] != "/":
+            name += "/"
+
+        if name == "../": #当前目录
+            self.now = self.now.parent  # 返回目录到上级
+            return
+        # 找到和name相同的文件
+        for child in self.now.children:
+            if child.name == name: 
+                self.now = child # 切换目录到child
+                return  # 输出
+        # 子目录中无该文件夹，报错
+        raise ValueError("invaild dir")
+    
+tree = FileSystemTree() # 创建树
+# 新建文件夹
+tree.mkdir("Var/")
+tree.mkdir("bin/")
+tree.mkdir("usr/")
+print(tree.ls()) # 展示当前子目录
+
+# 切换到子目录
+tree.cd("bin/")
+tree.mkdir("python/") # 子目录中创建文件夹
+print(tree.ls()) # 展示当前子目录
+
+# 切换回上级目录
+tree.cd("../")
+print(tree.ls()) # 展示当前子目录
+
