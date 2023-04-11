5e3beb1e6374b22038c4ae3c6fc118c007813b66
Limin <850778345@qq.com>
2023-04-11 20:02:01 +0800

    重命名为p66.py

---
+++ text.py
@@ -0,0 +1,9 @@
+class BiTreeNode(): # 二叉树节点
+    def __init__(self, data) -> None: # 属性
+        self.data = data # 树的值
+        self.lchild = None # 左孩子
+        self.rchild = None # 右孩子
+        self.parent = None # 父节点
+
+
+
